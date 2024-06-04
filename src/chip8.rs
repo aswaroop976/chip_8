523757df1a788b349efa933eab6998b58c4b9fc5
Arpan Swaroop <arpan.swaroop@gmail.com>
2024-06-04 18:08:08 -0500

    making my way through instructions, not many left to implement

--- src/chip8.rs
+++ src/chip8.rs
@@ -85,50 +85,144 @@ impl Chip8 {
     }
     // JP - 1NNN
     // Instruction: jump to address NNN
-    fn jp(&mut self, address: u16) {
-        self.program_counter = address
+    fn jp(&mut self, opcode: u16) {
+        let address = opcode & 0x0FFF;
+        self.program_counter = address;
     }
     // CALL - 2NNN
     // Instruction: call subroutine at NNN
-    fn call(&mut self, address: u16) {
+    fn call(&mut self, opcode: u16) {
+        let address = opcode & 0x0FFF;
         self.stack[self.stack_pointer as usize] = self.program_counter;
         self.stack_pointer += 1;
-        self.program_counter = address
+        self.program_counter = address;
     }
     // SE Vx, byte - 3XNN
     // Instruction: skip next instruction if Vx equals NN
-    fn se_vx_byte(&mut self, x: u16, byte: u8) {
-        if self.registers[x as usize] == byte {
+    fn se_vx_byte(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let byte = (opcode & 0x00FF) as u8;
+        if self.registers[x] == byte {
             self.program_counter += 2;
         }
     }
     // SNE Vx, byte - 4XNN
     // Instruction: skip next instruction if Vx doesn't equal NN
-    fn sne_vx_byte(&mut self, x: u16, byte: u8) {
-        if self.registers[x as usize] != byte {
+    fn sne_vx_byte(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let byte = (opcode & 0x00FF) as u8;
+        if self.registers[x] != byte {
             self.program_counter += 2;
         }
     }
     // SE Vx, Vy - 5XY0
     // Instruction: skip next instruction if Vx equals Vy
-    fn se_vx_vy(&mut self, x: u16, y: u16) {
-        if self.registers[x as usize] == self.registers[y as usize] {
+    fn se_vx_vy(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        if self.registers[x] == self.registers[y] {
             self.program_counter += 2;
         }
     }
     // LD Vx, byte - 6XNN
     // Instruction: set Vx to NN
-    fn ld_vx_byte(&mut self, x: u16, byte: u8) {
-        self.registers[x as usize] = byte;
+    fn ld_vx_byte(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let byte = (opcode & 0x00FF) as u8;
+        self.registers[x] = byte;
     }
     // ADD Vx, byte - 7XNN
     // Instruction: add NN to Vx
-    fn add_vx_byte(&mut self, x: u16, byte: u8) {
-        self.registers[x as usize] = self.registers[x as usize].wrapping_add(byte);
+    fn add_vx_byte(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let byte = (opcode & 0x00FF) as u8;
+        self.registers[x] = self.registers[x].wrapping_add(byte);
     }
     // LD Vx, Vy - 8XY0
     // Instruction: set Vx to the value of Vy
-    fn ld_vx_vy(&mut self, x: u16, y: u16) {
-        self.registers[x as usize] = self.registers[y as usize];
+    fn ld_vx_vy(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        self.registers[x] = self.registers[y];
+    }
+    // OR Vx, Vy - 8XY1
+    // Instruction: set Vx to Vx OR Vy
+    fn or_vx_vy(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        self.registers[x] |= self.registers[y];
+    }
+    // AND Vx, Vy - 8XY2
+    // Instruction: set Vx to Vx AND Vy
+    fn and_vx_vy(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        self.registers[x] &= self.registers[y];
+    }
+    // XOR Vx, Vy - 8XY3
+    // Instruction: set Vx to Vx XOR Vy
+    fn xor_vx_vy(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        self.registers[x] ^= self.registers[y];
+    }
+    // ADD Vx, Vy - 8XY4
+    // Instruction: Add Vy to Vx, set VF = carry
+    fn add_vx_vy(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        let (result, carry) = self.registers[x].overflowing_add(self.registers[y]);
+        self.registers[x] = result;
+        self.registers[0xF] = if carry { 1 } else { 0 };
+    }
+    // SUB Vx, Vy - 8XY5
+    // Instruction: subtract Vy from Vx, set VF = NOT borrow
+    fn sub_vx_vy(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        let (result, borrow) = self.registers[x].overflowing_sub(self.registers[y]);
+        self.registers[x] = result;
+        self.registers[0xF] = if borrow { 0 } else { 1 };
+    }
+    // SHR Vx - 8XY6
+    // Instruction: set Vx = Vx SHR 1
+    fn shr_vx(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        self.registers[0xF] = self.registers[x] & 0x1;
+        self.registers[x] >>= 1;
+    }
+    // SUBN Vx, Vy - 8XY7
+    // Instruction: set Vx = Vy - Vx, set VF = NOT borrow
+    fn subn_vx_vy(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        self.registers[0xF] = if self.registers[y] > self.registers[x] {
+            1
+        } else {
+            0
+        };
+        self.registers[x] = self.registers[y].wrapping_sub(self.registers[x]);
+    }
+    // SHL Vx - 8XYE
+    // Instruction: set Vx = Vx SHL 1
+    fn shl_vx(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        self.registers[0xF] = (self.registers[x] & 0x80) >> 7;
+        self.registers[x] <<= 1;
+    }
+    // SNE Vx, Vy - 9XY0
+    // Instruction: skip the next instruction if Vx != Vy
+    fn sne_vx_vy(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        if self.registers[x] != self.registers[y] {
+            self.program_counter += 2;
+        }
+    }
+    // LD I, addr - ANNN
+    // Instruction: set I = NNN
+    fn ld_i_addr(&mut self, opcode: u16) {
+        let address = opcode & 0x0FFF;
+        self.index_register = address;
     }
 }
